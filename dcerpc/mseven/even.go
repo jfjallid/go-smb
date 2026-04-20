f6f349f915fc20d673f1eefc62da6532b1bcf290
Jimmy Fjällid <jimmy@fjallid.com>
2026-04-20 20:40:54 +0200

    Added minimal support for MS-EVEN and MS-RPRN to coerce authentication.

---
+++ dcerpc/mseven/even.go
@@ -0,0 +1,162 @@
+// MIT License
+//
+// # Copyright (c) 2026 Jimmy Fjällid
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Package mseven implements a minimal client for the MS-EVEN (EventLog
+// Remoting Protocol) supporting ElfrOpenBELW and ElfrCloseEL.
+package mseven
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/jfjallid/go-smb/dcerpc"
+	"github.com/jfjallid/golog"
+	"github.com/jfjallid/mstypes"
+)
+
+var (
+	log                  = golog.Get("github.com/jfjallid/go-smb/dcerpc/mseven")
+	le  binary.ByteOrder = binary.LittleEndian
+)
+
+const (
+	MSRPCUuidEven                = "82273FDC-E32A-18C3-3F78-827929DC23EA"
+	MSRPCEvenPipe                = "eventlog"
+	MSRPCEvenMajorVersion uint16 = 0
+	MSRPCEvenMinorVersion uint16 = 0
+)
+
+// Operation codes
+const (
+	OpElfrCloseEL  uint16 = 2
+	OpElfrOpenBELW uint16 = 9
+)
+
+// NTSTATUS error codes
+const (
+	StatusSuccess          uint32 = 0x00000000
+	StatusAccessDenied     uint32 = 0x00000005
+	StatusInvalidParameter uint32 = 0x00000057
+	StatusInvalidHandle    uint32 = 0x00000006
+)
+
+var ResponseCodeMap = map[uint32]error{
+	StatusAccessDenied:     fmt.Errorf("ERROR_ACCESS_DENIED"),
+	StatusInvalidParameter: fmt.Errorf("ERROR_INVALID_PARAMETER"),
+	StatusInvalidHandle:    fmt.Errorf("ERROR_INVALID_HANDLE"),
+}
+
+type RPCCon struct {
+	*dcerpc.ServiceBind
+}
+
+func NewRPCCon(sb *dcerpc.ServiceBind) *RPCCon {
+	return &RPCCon{ServiceBind: sb}
+}
+
+// ElfrOpenBELW opens a handle to a backup event log file on the server.
+// backupFileName is the path to the backup event log file on the remote system.
+func (c *RPCCon) ElfrOpenBELW(backupFileName string) (handle [20]byte, err error) {
+	log.Traceln("In ElfrOpenBELW")
+
+	nameLen := uint16(len(backupFileName) * 2)
+	req := elfrOpenBELWReq{
+		UNCServerName: nil,
+		BackupFileName: mstypes.RPCUnicodeString{
+			Length:        nameLen,
+			MaximumLength: nameLen,
+			Value:         backupFileName,
+		},
+		MajorVersion: 1,
+		MinorVersion: 1,
+	}
+
+	buf, err := req.Marshal()
+	if err != nil {
+		log.Errorln(err)
+		return handle, err
+	}
+
+	result, err := c.MakeRequest(OpElfrOpenBELW, buf)
+	if err != nil {
+		log.Errorln(err)
+		return handle, err
+	}
+
+	var resp elfrOpenBELWRes
+	if err = resp.Unmarshal(result); err != nil {
+		log.Errorln(err)
+		return handle, err
+	}
+
+	if resp.ReturnCode != StatusSuccess {
+		err = responseError("ElfrOpenBELW", resp.ReturnCode)
+		log.Errorln(err)
+		return handle, err
+	}
+
+	handle = resp.LogHandle
+	return
+}
+
+// ElfrCloseEL closes an event log handle obtained from ElfrOpenBELW.
+func (c *RPCCon) ElfrCloseEL(handle *[20]byte) error {
+	log.Traceln("In ElfrCloseEL")
+
+	req := elfrCloseELReq{
+		LogHandle: *handle,
+	}
+
+	buf, err := req.Marshal()
+	if err != nil {
+		log.Errorln(err)
+		return err
+	}
+
+	result, err := c.MakeRequest(OpElfrCloseEL, buf)
+	if err != nil {
+		log.Errorln(err)
+		return err
+	}
+
+	var resp elfrCloseELRes
+	if err = resp.Unmarshal(result); err != nil {
+		log.Errorln(err)
+		return err
+	}
+
+	if resp.ReturnCode != StatusSuccess {
+		err = responseError("ElfrCloseEL", resp.ReturnCode)
+		log.Errorln(err)
+		return err
+	}
+
+	*handle = resp.LogHandle
+	return nil
+}
+
+func responseError(method string, code uint32) error {
+	if errMsg, ok := ResponseCodeMap[code]; ok {
+		return fmt.Errorf("%s returned error: %v", method, errMsg)
+	}
+	return fmt.Errorf("%s returned unknown error: 0x%08x", method, code)
+}
---
+++ dcerpc/msrprn/structures.go
@@ -0,0 +1,230 @@
+// MIT License
+//
+// # Copyright (c) 2026 Jimmy Fjällid
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package msrprn
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+
+	"github.com/jfjallid/ndr"
+)
+
+// devModeContainerNDR represents DEVMODE_CONTAINER (MS-RPRN 2.2.1.3.1).
+// For our use case, always empty (cbBuf=0, pDevMode=NULL).
+type devModeContainerNDR struct {
+	CbBuf    uint32
+	PDevMode *devModeDataNDR `ndr:"fullpointer"` // [unique] embedded pointer
+}
+
+type devModeDataNDR struct {
+	Data []byte `ndr:"conformant"`
+}
+
+// splClientContainerNDR represents SPLCLIENT_CONTAINER (MS-RPRN 2.2.1.3.9).
+// Non-encapsulated union with Level as discriminant.
+type splClientContainerNDR struct {
+	Level uint32            `ndr:"unionTag"`
+	Info1 splClientInfo1NDR `ndr:"unionField,pointer"`
+}
+
+// SwitchFunc implements the ndr.Union interface for SPLCLIENT_CONTAINER.
+func (s splClientContainerNDR) SwitchFunc(t interface{}) string {
+	level, ok := t.(uint32)
+	if !ok {
+		return ""
+	}
+	switch level {
+	case 1:
+		return "Info1"
+	}
+	return ""
+}
+
+// splClientInfo1NDR represents SPLCLIENT_INFO_1 (MS-RPRN 2.2.1.3.7).
+type splClientInfo1NDR struct {
+	DwSize                 uint32
+	PMachineName           string `ndr:"fullpointer,conformant,varying"`
+	PUserName              string `ndr:"fullpointer,conformant,varying"`
+	DwBuildNum             uint32
+	DwMajorVersion         uint32
+	DwMinorVersion         uint32
+	WProcessorArchitecture uint16
+}
+
+// conformantByteArrayNDR wraps a conformant byte array for use as a nullable pointer target.
+type conformantByteArrayNDR struct {
+	Data []byte `ndr:"conformant"`
+}
+
+// RpcV2NotifyOptions represents RPC_V2_NOTIFY_OPTIONS (MS-RPRN 2.2.1.13.1).
+// Exported for callers who need non-NULL pOptions (rare).
+type RpcV2NotifyOptions struct {
+	Version  uint32
+	Reserved uint32
+	Count    uint32
+}
+
+// rpcV2NotifyOptionsNDR is the NDR wire representation of RPC_V2_NOTIFY_OPTIONS.
+type rpcV2NotifyOptionsNDR struct {
+	Version  uint32
+	Reserved uint32
+	Count    uint32
+	PTypes   uint32 // NULL pointer (we don't support pTypes)
+}
+
+// --- RpcOpenPrinterEx (Opnum 69) ---
+
+type rpcOpenPrinterExReqNDR struct {
+	PrinterName      *string               `ndr:"toplevel,fullpointer,conformant,varying"` // [in, string, unique]
+	PDataType        *string               `ndr:"toplevel,fullpointer,conformant,varying"` // [in, string, unique] - always NULL
+	DevModeContainer devModeContainerNDR   // [in]
+	AccessRequired   uint32                // [in]
+	ClientInfo       splClientContainerNDR // [in]
+}
+
+type rpcOpenPrinterExResNDR struct {
+	Handle     [20]byte // PRINTER_HANDLE
+	ReturnCode uint32
+}
+
+// --- RpcClosePrinter (Opnum 29) ---
+
+type rpcClosePrinterReqNDR struct {
+	Handle [20]byte
+}
+
+type rpcClosePrinterResNDR struct {
+	Handle     [20]byte // zeroed by server
+	ReturnCode uint32
+}
+
+// --- RpcRemoteFindFirstPrinterChangeNotification (Opnum 62) ---
+
+type rpcRFFPCNReqNDR struct {
+	Handle          [20]byte
+	FdwFlags        uint32
+	FdwOptions      uint32
+	PszLocalMachine *string `ndr:"toplevel,fullpointer,conformant,varying"` // [in, string, unique]
+	DwPrinterLocal  uint32
+	CbBuffer        uint32
+	PBuffer         *conformantByteArrayNDR `ndr:"toplevel,fullpointer"` // [in, out, unique] - NULL
+}
+
+type rpcRFFPCNResNDR struct {
+	PBuffer    *conformantByteArrayNDR `ndr:"toplevel,fullpointer"` // [in, out]
+	ReturnCode uint32
+}
+
+// --- RpcRemoteFindFirstPrinterChangeNotificationEx (Opnum 65) ---
+
+type rpcRFFPCNExReqNDR struct {
+	Handle          [20]byte
+	FdwFlags        uint32
+	FdwOptions      uint32
+	PszLocalMachine *string `ndr:"toplevel,fullpointer,conformant,varying"` // [in, string, unique]
+	DwPrinterLocal  uint32
+	POptions        *rpcV2NotifyOptionsNDR `ndr:"toplevel,fullpointer"` // [in, unique] - NULL
+}
+
+type rpcRFFPCNExResNDR struct {
+	ReturnCode uint32
+}
+
+// Marshal/Unmarshal methods
+
+func (s *rpcOpenPrinterExReqNDR) Marshal() ([]byte, error) {
+	enc := ndr.NewEncoder(bytes.NewBuffer([]byte{}), false)
+	enc.SetEndianness(binary.LittleEndian)
+	b, err := enc.Encode(s)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling RpcOpenPrinterExReq: %v", err)
+	}
+	return b, nil
+}
+
+func (s *rpcOpenPrinterExResNDR) Unmarshal(b []byte) error {
+	dec := ndr.NewDecoder(bytes.NewReader(b), false)
+	err := dec.Decode(s)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling RpcOpenPrinterExRes: %v", err)
+	}
+	return nil
+}
+
+func (s *rpcClosePrinterReqNDR) Marshal() ([]byte, error) {
+	enc := ndr.NewEncoder(bytes.NewBuffer([]byte{}), false)
+	enc.SetEndianness(binary.LittleEndian)
+	b, err := enc.Encode(s)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling RpcClosePrinterReq: %v", err)
+	}
+	return b, nil
+}
+
+func (s *rpcClosePrinterResNDR) Unmarshal(b []byte) error {
+	dec := ndr.NewDecoder(bytes.NewReader(b), false)
+	err := dec.Decode(s)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling RpcClosePrinterRes: %v", err)
+	}
+	return nil
+}
+
+func (s *rpcRFFPCNReqNDR) Marshal() ([]byte, error) {
+	enc := ndr.NewEncoder(bytes.NewBuffer([]byte{}), false)
+	enc.SetEndianness(binary.LittleEndian)
+	b, err := enc.Encode(s)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling RpcRemoteFindFirstPrinterChangeNotificationReq: %v", err)
+	}
+	return b, nil
+}
+
+func (s *rpcRFFPCNResNDR) Unmarshal(b []byte) error {
+	dec := ndr.NewDecoder(bytes.NewReader(b), false)
+	err := dec.Decode(s)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling RpcRemoteFindFirstPrinterChangeNotificationRes: %v", err)
+	}
+	return nil
+}
+
+func (s *rpcRFFPCNExReqNDR) Marshal() ([]byte, error) {
+	enc := ndr.NewEncoder(bytes.NewBuffer([]byte{}), false)
+	enc.SetEndianness(binary.LittleEndian)
+	b, err := enc.Encode(s)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling RpcRemoteFindFirstPrinterChangeNotificationExReq: %v", err)
+	}
+	return b, nil
+}
+
+func (s *rpcRFFPCNExResNDR) Unmarshal(b []byte) error {
+	dec := ndr.NewDecoder(bytes.NewReader(b), false)
+	err := dec.Decode(s)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling RpcRemoteFindFirstPrinterChangeNotificationExRes: %v", err)
+	}
+	return nil
+}
